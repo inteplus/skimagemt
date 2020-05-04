2df1e229d0f33fd7e62bb096d3c45054f6ab2316
Minh-Tri Pham <pmtri80@gmail.com>
2020-05-04 08:53:36 +0100

    mt.skimage instead of skimagemt

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_packages
-from skimagemt.version import version
+from setuptools import setup, find_namespace_packages
+from mt.skimage.version import version
 
 setup(
     name='skimagemt',
     version=version,
     description="Minh-Tri Pham's extra modules for scikit-image",
     author=["Minh-Tri Pham"],
-    packages=find_packages(),
-    package_data={
-        'skimagemt': ['*.pyx'],
-    },
-    include_package_data=True,
-    zip_safe=False,
+    packages=find_namespace_packages(include=['ml.*']),
+    #package_data={
+    #    'skimagemt': ['*.pyx'],
+    #},
+    #include_package_data=True,
+    #zip_safe=False,
     install_requires=[
         'scikit-image>=0.14.0',
         'geomt>=0.1.1',
